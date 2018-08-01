a85dc82995b72feafb231de931935887c14f2cca
matheusbsilva <matheusbattista@hotmail.com>
2018-08-01 15:18:30 -0300

    Create jumpTo to go back to any step in history

--- src/index.js
+++ src/index.js
@@ -53,12 +53,14 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0,
+      this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice()
 
@@ -69,13 +71,32 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{squares: squares,}]),
       xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
     });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares)
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -95,7 +116,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
